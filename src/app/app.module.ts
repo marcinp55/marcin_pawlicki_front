91ee33b62dd69db1bde87ca5673a87f6f998ed67
“marcinp55 <marcinp55@gmail.com>
2019-03-22 21:14:06 +0100

    View only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { CalculatorEditorComponent } from './calculator/calculator-editor/calculator-editor.component';
+import { CalculatorDisplayComponent } from './calculator/calculator-display/calculator-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CalculatorComponent,
+    CalculatorEditorComponent,
+    CalculatorDisplayComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/calculator/calculator-editor/calculator-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-editor',
+  templateUrl: './calculator-editor.component.html',
+  styleUrls: ['./calculator-editor.component.css']
+})
+export class CalculatorEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
